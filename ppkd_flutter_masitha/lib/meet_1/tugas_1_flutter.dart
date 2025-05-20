272e2cc0347f6c803f3400a65ea96ae5dd6c35ba
masithagaluh27 <projectmasitha.com>
2025-05-20 08:42:38 +0700

    tugas 1 flutter

--- ppkd_flutter_masitha/lib/meet_1/tugas_1_flutter.dart
+++ ppkd_flutter_masitha/lib/meet_1/tugas_1_flutter.dart
@@ -7,42 +7,35 @@ class Tugas1Flutter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 205, 161, 255), 
-        title: Text("PROFIL SAYA"), // Changed title to "Profil Saya"
-        centerTitle: true, 
+        backgroundColor: const Color.fromARGB(255, 205, 161, 255),
+        title: const Text("PROFIL SAYA" ,),
+        centerTitle: true,
       ),
-      drawer: Drawer(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(children: [Text("Gambar 1")]),
-            Row(
-              
-            )
-          ],
-        ),
-      ),
-      backgroundColor: Colors.grey[100], // Changed background color to white
+      backgroundColor: Colors.grey[100], // Background color
       floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromARGB(255, 175, 146, 241),
-        child: Icon(Icons.add_link, color: Colors.white),
-        onPressed: () {},
+        child: const Icon(Icons.add_link, color: Colors.white),
+        onPressed: () {}, // Kosongkan dulu
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+        children: const [
           Text(" "),
           Text(" "),
-          Text("Nama Lengkap : Masitha Ayu Galuh Ramadhani", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),),
-          Text (" "),
-          Row(children: 
-          [Icon(Icons.location_city_rounded , color: const Color.fromARGB(255, 229, 173, 255),), Text("Jakarta Selatan" , style:TextStyle (fontSize: 18, fontWeight: FontWeight.w400)),],),
-          Text (" "),
-          Text("Deskripsi singkat tentang saya : ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),),
-          Text("      Seorang pelajar yang sedang belajar Flutter", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),),
-        ]
+          Text("Nama Lengkap : Masitha Ayu G.R", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+          Text(" "),
+          Row(
+            children: [
+              Icon(Icons.location_city_rounded, color: Color.fromARGB(255, 229, 173, 255)),
+              Text("DK Jakarta Selatan", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+            ],
+          ),
+          Text(" "),
+          Text("Deskripsi singkat tentang saya : ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+          Text("      Seorang pelajar yang sedang belajar Flutter.", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+        ],
       ),
     );
   }
