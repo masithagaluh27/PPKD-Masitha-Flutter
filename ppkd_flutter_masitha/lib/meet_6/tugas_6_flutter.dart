930d7c03cd216598f83f903c478dab1388e1c239
masithagaluh27 <projectmasitha.com>
2025-05-26 11:41:05 +0700

    Tugas 6 Flutter

--- ppkd_flutter_masitha/lib/meet_6/tugas_6_flutter.dart
+++ ppkd_flutter_masitha/lib/meet_6/tugas_6_flutter.dart
@@ -1,32 +1,298 @@
 import 'package:flutter/material.dart';
 
-class TugasEnamFlutter extends StatelessWidget {
-  final TextEditingController emailcontroller = TextEditingController();
+void main() {
+  runApp(const MaterialApp(home: TugasEnamFlutter()));
+}
 
-  TugasEnamFlutter({super.key});
+class TugasEnamFlutter extends StatefulWidget {
+  const TugasEnamFlutter({super.key});
+
+  @override
+  State<TugasEnamFlutter> createState() => _TugasEnamFlutterState();
+}
+
+class _TugasEnamFlutterState extends State<TugasEnamFlutter> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(24),
+          padding: const EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 20),
-              Icon(Icons.arrow_back),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 20),
+
+              // Panah dan tulisan Login
+              Row(
+                children: const [
+                  Icon(Icons.arrow_back_ios),
+                  SizedBox(width: 20),
+                  Text(
+                    "Login",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 40),
+
+              // Tulisan Welcome Back
+              const Text(
                 "Welcome Back",
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
               ),
-              Text("sign in to your account"),
-              SizedBox(height: 24),
+              const SizedBox(height: 16),
+
+              //Tulisan Sign in to your account
+              const Text(
+                "Sign in to your account",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(height: 35),
+
+              // text di atas Email TextField
+              const Text(
+                'Email Address',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color(0xff888888),
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              // Email TextField
               TextField(
-                controller: emailcontroller,
-                decoration: InputDecoration(),
+                controller: emailController,
+                decoration: InputDecoration(
+                  hintText: 'Projectmasitha@gmail.com',
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
               ),
+              const SizedBox(height: 16),
+
+              // text di atas Password TextField
+              const Text(
+                'Password',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color(0xff888888),
+                ),
+              ),
+              const SizedBox(height: 8),
+
+              // Password TextField
+              TextField(
+                controller: passwordController,
+                obscureText: !isPasswordVisible,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility_outlined
+                          : Icons.visibility_off_outlined,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+
+              // Forgot Password
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xffEA9459),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Tombol Login
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: const StadiumBorder(),
+                    backgroundColor: Colors.blue.shade900,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              const SizedBox(height: 8),
+
+              // Join Us Text
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    // tulis disini jika mau diarahkan ke page sign up
+                  },
+                  child: RichText(
+                    text: const TextSpan(
+                      text: "Don't have an account? ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Sign Up',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color(0xffEA9459),
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+
+              // Or Sign In With
+              Row(
+                children: const [
+                  Expanded(child: Divider(thickness: 1)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "Or Sign In With",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Divider(thickness: 1)),
+                ],
+              ),
+              const SizedBox(height: 35),
+
+              // Tombol Google & Facebook
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Tombol Google
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Image.asset(
+                        "assets/image/iconGoogle.PNG",
+                        height: 24,
+                      ),
+                      label: const Text(
+                        "Google",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFAFAFA),
+                        elevation: 0,
+                        shape: const StadiumBorder(),
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                        shadowColor: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+
+                  // Tombol Facebook
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Image.asset(
+                        "assets/image/logo facebook.PNG",
+                        height: 24,
+                      ),
+                      label: const Text(
+                        "Facebook",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFAFAFA),
+                        elevation: 0,
+                        shape: const StadiumBorder(),
+                        padding: const EdgeInsets.symmetric(vertical: 14),
+                        shadowColor: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: RichText(
+                    text: const TextSpan(
+                      text: "Don't have an account? ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Join Us',
+                          style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 30),
             ],
           ),
         ),
