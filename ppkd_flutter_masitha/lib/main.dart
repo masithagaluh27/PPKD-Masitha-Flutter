ed166b302dfed8f0786472c70ba787287bc4c106
masithagaluh27 <projectmasitha@gmail.com>
2025-05-27 21:44:38 +0700

    materi

--- ppkd_flutter_masitha/lib/main.dart
+++ ppkd_flutter_masitha/lib/main.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:ppkd_flutter_masitha/meet_11/meet_11a.dart';
+import 'meet_5/tugas_5_flutter.dart';
+import 'meet_6/tugas_6_flutter.dart';
+import 'meet_2/tugas_2_flutter.dart';
+import 'meet_3/meet_3a.dart';
 
-// import 'package:ppkd_flutter_masitha/meet_2/contoh.dart';
-// import 'package:ppkd_flutter_masit
-// ha/meet_2/meet_2.dart';
 
-void main() {
-  runApp(const MyApp()); // tidak boleh di ganti ganti
+void main() {} class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        //Harus didaftarkan dulu disini
+        "/": (context) => (),TugasEnamFlutter
+        "/meet_2": (context) => Tugas5Flutter(),
+        MeetTigaA.id: (context) => Tugas2Flutter(),
+      },
       debugShowCheckedModeBanner: false,
-      title: 'Tugas 6 Flutter', // nama aplikasi
+      title: 'PPKD B 2',
+
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffFFFFFF)),
+        // useMaterial3: true,
+        fontFamily: 'Poppins',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {TargetPlatform.android: CupertinoPageTransitionsBuilder()},
+        ),
       ),
-      home: MeetSebelasA(),
+      home: MeetLima(),
+      // home: LoginScreen(),
     );
   }
 }
